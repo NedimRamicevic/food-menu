6eb6952366b2a0c5c30a1106ca62699994051dc0
Nedim <mr.ramicevic@gmail.com>
2021-09-07 09:47:47 +0300

    foodCard2

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-function FoodCard() {
+function FoodCard({cardInfo}) {
     return (
         <div>
             <div className="photo" >
-                <img src="http://placehold.it/" alt=""></img>
+                <img src={cardInfo.img} alt=""></img>
             </div>
-            <div className="menu-title">Title</div>
-            <div className="menu-info">Info</div>
+            <div className="menu-title">
+                <h2>{cardInfo.title}</h2>
+            </div>
+            <div className="menu-info">
+                <p>{cardInfo.desc}</p>
+            </div>
         </div>
     )
 }
