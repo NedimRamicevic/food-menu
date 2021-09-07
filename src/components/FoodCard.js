82331a78a251367394dbff190a6a5c53c60d94e4
Nedim <mr.ramicevic@gmail.com>
2021-09-07 09:43:12 +0300

    foodCard

---
+++ src/components/FoodCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function FoodCard() {
+    return (
+        <div>
+            <div className="photo" >
+                <img src="http://placehold.it/" alt=""></img>
+            </div>
+            <div className="menu-title">Title</div>
+            <div className="menu-info">Info</div>
+        </div>
+    )
+}
+
+export default FoodCard
---
+++ src/components/ListCards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListCards() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ListCards
